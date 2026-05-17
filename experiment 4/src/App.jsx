6e04a3aa3fafce89d3355a5945984c1c65d57804
NAITIK RAJPUT <naitikrajput.26.04@gmail.com>
2026-05-17 14:08:33 +0530

    exp 4

--- experiment 4/src/App.jsx
+++ experiment 4/src/App.jsx
@@ -10,7 +10,11 @@ function App() {
       <Student name="Naitik rajput" course="Computer Science" marks="95" />
       <Student name="Vani sharma" course="Information Technology" marks="92" />
       <Student name="tushar" course="Electronics" marks="78" />
+
       
+      <h4><b>Name: NAITIK RAJPUT</b></h4>
+            <h4><b>Addmission no.: 2025B01010719</b></h4>
+            <h4><b>Class: CSE 24</b></h4>
     </div>
   );
 }
